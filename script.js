e80895a3e3220271e17f2893a7f5fe730f485f71
Eli0uce <93868767+Eli0uce@users.noreply.github.com>
2023-06-27 00:34:13 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     // Afficher une alerte personnalisée avec du texte et une image
     const message = 'Merci pour tes coordonnées et mots de passe.';
-    const imageUrl = './miles.gif';
+    const imageUrl = 'miles.gif';
 
     Swal.fire({
       title: 'Hop la, tu viens de te faire hacker !',
